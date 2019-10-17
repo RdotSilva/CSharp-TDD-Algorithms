140b17dd308442d007cafe31a2164330baa42ae0
Ryan Silva <RyandotSilva@gmail.com>
2019-10-17 11:03:46 -0400

    Add FizzBuzzTests

---
+++ TDD-Algorithms/Tests/FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TDD_Algorithms.Tests.FizzBuzzTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase("Fizz", 3)]
+        [TestCase("Fizz", 6)]
+        [TestCase("Buzz", 5)]
+        [TestCase("Buzz", 10)]
+        [TestCase("FizzBuzz", 15)]
+        [TestCase("FizzBuzz", 30)]
+        [TestCase("", 7)]
+        [TestCase("", 11)]
+
+        public void TestFizzBuzz(string expected, int number)
+        {
+            Assert.AreEqual(expected, FizzBuzz(number));
+        }
+
+        private string FizzBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+            if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
+            
+            return "";
+        }
+    }
+}
