045039a4c650c395c5c71104e54bc71f2ce0ce29
Ryan Silva <RyandotSilva@gmail.com>
2019-10-17 10:46:18 -0400

    Add project files.

---
+++ TDD-Algorithms/Tests/FibonacciTests/FibonacciTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDD_Algorithms.Tests.FibonacciTests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+
+        public void TestFibonacci(int expected, int index)
+        {
+            Assert.AreEqual(expected, GetFibonacci(index));
+        }
+
+        private int GetFibonacci(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    return GetFibonacci(index - 1) + GetFibonacci(index - 2);
+            }
+        }
+    }
+}
