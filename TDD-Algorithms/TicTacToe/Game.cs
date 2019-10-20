a374cf3997c81789b45c31b226ab728f7441a32b
Ryan Silva <RyandotSilva@gmail.com>
2019-10-20 16:16:18 -0400

    Add board state to Game class

--- TDD-Algorithms/TicTacToe/Game.cs
+++ TDD-Algorithms/TicTacToe/Game.cs
@@ -5,12 +5,26 @@ namespace TDD_Algorithms.TicTacToe
     public class Game
     {
         public int MovesCounter { get; private set; }
+        private readonly State[] _board = new State[9];
+
+        public Game()
+        {
+            for (var i = 0; i < _board.Length; i++)
+            {
+                _board[i] = State.Unset;
+            }
+        }
 
         public void MakeMove(int index)
         {
             if (index < 1 || index > 9)
                 throw new ArgumentOutOfRangeException();
+            if(GetState(index) != State.Unset)
+                throw new InvalidOperationException();
+
             MovesCounter++;
         }
+
+        
     }
 }
