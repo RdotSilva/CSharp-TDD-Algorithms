1cfb4c11bc564ab042dfb71cd697cfadf66653c9
Ryan Silva <RyandotSilva@gmail.com>
2019-10-20 18:17:11 -0400

    Add GetPrintableState method

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -25,7 +25,23 @@ namespace TicTacToeGame
             Console.WriteLine($"Result: {g.GetWinner()}");
             Console.ReadLine();
         }
+        private static string GetPrintableState()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 1; i <= 7; i += 3)
+            {
+                sb.AppendLine("     |     |     ")
+                    .AppendLine(
+                        $"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  ")
+                    .AppendLine("_____|_____|_____|");
+            }
+
+            return sb.ToString();
+        }
 
         
+
+
     }
 }
