29f36bbb48b28314972073981e053b64314b1d7c
Ryan Silva <RyandotSilva@gmail.com>
2019-10-21 19:16:56 -0400

    Add IStack interface

--- TDD-Algorithms/Tests/ImmutableStackTests/ImmutableStackTests.cs
+++ TDD-Algorithms/Tests/ImmutableStackTests/ImmutableStackTests.cs
@@ -5,8 +5,17 @@ using NUnit.Framework;
 
 namespace TDD_Algorithms.Tests.ImmutableStackTests
 {
+    public interface IStack<T>
+    {
+        IStack<T> Push(T value);
+        IStack<T> Pop();
+        T Peek();
+        bool IsEmpty { get; }
+    }
+
     [TestFixture]
     public class ImmutableStackTests
     {
+
     }
 }
