cee08718c3a63feddacc90e0ae1c64bc9035b2b4
Ryan Silva <RyandotSilva@gmail.com>
2019-10-22 16:38:28 -0400

    Add AddFirst_HeadAndTailAreSame test

--- TDD-Algorithms/Tests/LinkedListTests/LinkedListTests.cs
+++ TDD-Algorithms/Tests/LinkedListTests/LinkedListTests.cs
@@ -16,13 +16,41 @@ namespace TDD_Algorithms.Tests.LinkedListTests
             Assert.AreEqual(1, node.Value);
             Assert.IsNull(node.Next);
         }
+
+        [Test]
+        public void AddFirst_HeadAndTailAreSame()
+        {
+
+            MyLinkedList<int> list = new MyLinkedList<int>();
+            list.AddFirst(1);
+
+            Assert.AreEqual(1, list.Head.Value);
+            Assert.AreEqual(1, list.Tail.Value);
+            Assert.AreSame(list.Head, list.Tail);
+        }
+    }
+
+    public class MyLinkedList<T>
+    {
+        public ListNode<T> Tail { get; private set; }
+        public ListNode<T> Head { get; private set; }
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new ListNode<T>(value));
+        }
+
+        private void AddFirst(ListNode<T> node)
+        {
+            Tail = Head = node;
+        }
     }
 
     public class ListNode<T>
     {
         public ListNode(T value)
         {
-            throw new NotImplementedException();
+            Value = value;
         }
 
         public ListNode<T> Next { get; set; }
