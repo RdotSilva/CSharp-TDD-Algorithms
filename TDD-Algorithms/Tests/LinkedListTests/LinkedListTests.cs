5d3c1f2675cdea3b735dc9153c660bd0103a3615
Ryan Silva <RyandotSilva@gmail.com>
2019-10-22 16:29:07 -0400

    Add CreateNode_SetsValueAndNextIsNull test

--- TDD-Algorithms/Tests/LinkedListTests/LinkedListTests.cs
+++ TDD-Algorithms/Tests/LinkedListTests/LinkedListTests.cs
@@ -8,5 +8,24 @@ namespace TDD_Algorithms.Tests.LinkedListTests
     [TestFixture]
     public class LinkedListTests
     {
+        [Test]
+        public void CreateNode_SetsValueAndNextIsNull()
+        {
+            ListNode<int> node = new ListNode<int>(1);
+
+            Assert.AreEqual(1, node.Value);
+            Assert.IsNull(node.Next);
+        }
+    }
+
+    public class ListNode<T>
+    {
+        public ListNode(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ListNode<T> Next { get; set; }
+        public T Value { get; set; }
     }
 }
